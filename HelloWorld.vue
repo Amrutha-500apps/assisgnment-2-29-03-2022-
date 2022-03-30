699b7eec55916c15073cbe0d9d257ad10eb19588
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-03-30 16:16:13 +0530

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <p> Amrutha</p>
+    <child></child>
+    <Question></Question>
+    <Table></Table>
+  </div>
+</template>
+<script>
+
+import child from '../components/child'
+import Question from'../components/Q1'
+import Table from'../components/Q2'
+export default {
+  name: 'HelloS33',
+  components:{
+    child,
+    Question,
+    Table,
+  },
+  props :{
+    Name:String
+   }
+}
+</script>
